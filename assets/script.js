4a2a0d946c0c7745ceb29fae73d43f96c3724656
SLEEPlESS <rosas_u@ymail.com>
2022-01-20 19:45:24 -0800

    made divs for questions and hid them and used onclick to show them

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
+var startButton = document.querySelector("#start-button");
+// var mainEl = document.querySelector()
+var startQuizZoneEl = document.querySelector("#start-quiz-zone");
+var questionZoneEl = document.querySelector("#question-zone");
+var endQuizZoneEl = document.querySelector("#end-quiz-zone");
+var promptDiv = document.querySelector("#question-choices");
 // make an array of objects with an array in the object to form the questions and answers
 var questions = [
   {
@@ -24,16 +30,23 @@ var questions = [
 }
 ];
 var currentQuestionIndex = 0;
+var timeLeft = 60;
 var timerEl = document.getElementById("timer")
-var startButton = document.querySelector("#start-button");
-
+questionZoneEl.style.display = "none";
+endQuizZoneEl.style.display = "none";
 // var timeLeft = questions.length * 15;
 
+function startQuiz(){
+  startTimer();
+  startQuizZoneEl.style.display = "none";
+  questionZoneEl.style.display = "block";
+  getQuestion();
+};
+
 // make a timer
-// function to run questions
 function startTimer(){ 
     console.log("starting");
-    var timeLeft = 10;
+   
     var timeInterval = setInterval(function() {
   if(timeLeft > 1) {
     timerEl.textContent = "Timer:" + timeLeft; 
@@ -42,6 +55,7 @@ function startTimer(){
       timerEl.textContent = 'Out of time';
       clearInterval(timeInterval);  
       displayMessage();
+      endQuiz();
     }
     
 }, 1000);
@@ -55,30 +69,51 @@ function getQuestion() {
     var titleEl = document.getElementById("question-title");
     titleEl.textContent = currentQuestion.title;
     // display choices in form of buttons
+    promptDiv.innerHTML = "";
     for(var i = 0; i < currentQuestion.choices.length; i++ ){
-      var promptDiv = document.querySelector(".prompt-style");
+      
       var answerBtnEl =  document.createElement("button");
       answerBtnEl.className = "btn";
       answerBtnEl.textContent = currentQuestion.choices[i];
+      answerBtnEl.onclick = questionClick;
       promptDiv.appendChild(answerBtnEl);
+    
     }
+    
     // clear out old questions and choices   
 
+
 };
-getQuestion();
 
 function questionClick() {
 // check if the user guessed wrong
 // display new time on page if user guesses wrong
 // move to the next question if user guesses at all
 // logic to check if there are any questions left
+  var coerrectAnswer = questions[currentQuestionIndex].answer;
+  var selectedAnswer = this.textContent;
+  console.log(selectedAnswer,coerrectAnswer)
+  if(coerrectAnswer !== selectedAnswer){
+    timeLeft -= 10;
+  }
+  currentQuestionIndex ++;
+  if(currentQuestionIndex === questions.length){
+    endQuiz();
+  }else {
+    getQuestion();
+  }
+  
 };
 
 function displayMessage(){
-    
+    //select paerent
+    // make <h1> with text stating that you are out of time 
 }
 
-
+  function endQuiz (){
+    questionZoneEl.style.display = "none";
+    endQuizZoneEl.style.display = "block";
+  };
 // save the score in local storage
 
-startButton.addEventListener('click', startTimer);
+startButton.addEventListener('click', startQuiz);
